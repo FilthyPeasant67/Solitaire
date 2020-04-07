1770b06001bfa28999d654fcfc0bbeeb889890e2
AlexG1031 <42430291+AlexG1031@users.noreply.github.com>
2020-04-07 14:46:08 -0400

    Blackjack working setup code #2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,63 +1,93 @@
 package sample;
 
 public class Main {
-        public static void main(String[] args) {
-
-            Deck deck = new Deck();
-            for (int i = 0; i < 52; i++) {
-                Card card = deck.getCardAt(i);
-                System.out.println(card.value + " of " + card.suit);
-            }
-            // Main logic for blackjack
-            // 1. Lets have a title screen
-            //        // 2. how much moneys do you want to put down?
-            //        // 3. Dealer gets two cards and user gets two cards
-            //        // 4. Determine whether to hit, split, hold, double, insurance
-            //        // 5. Dealer does the hittings
-            //        // 6. Either give a win screen or lose screen. Be sure to either take away money or give money bet
-            int phase = 1;
-            while (1 > 0) {
-                if (phase == 1) {
+    public static void main(String[] args) {
+        // Main logic for blackjack
+        // 1. Lets have a title screen
+        // 2. how much moneys do you want to put down?
+        // 3. Dealer gets two cards and user gets two cards
+        // 4. Determine whether to hit, split, hold, double, insurance
+        // 5. Dealer does the hittings
+        // 6. Either give a win screen or lose screen. Be sure to either take away money or give money bet
+        int phase = 1;
+        while (1 > 0) {
+            switch(phase) {
+                case 1:
                     executeTitleScreen();
                     phase = 2;
-                } else if (phase == 2) {
+                    break;
+                case 2:
                     executeBettingScreen();
                     phase = 3;
-                } else if (phase == 3) {
+                    break;
+                case 3:
                     initiateBlackjack();
                     phase = 4;
-                } else if (phase == 4) {
+                    break;
+                case 4:
                     playerPhase();
                     phase = 5; // either 5 or 6
-                } else if (phase == 5) {
+                    break;
+                case 5:
                     cpuPhase();
                     phase = 6;
-                } else if (phase == 6) {
+                    break;
+                case 6:
                     resultsPhase();
                     phase = 2;
+                    break;
                 }
             }
         }
 
-        public static void executeTitleScreen() {
+    /**
+     * Show title screen for blackjack
+     */
+    public static void executeTitleScreen() {
             // insert code here
-        }
-        public static void executeBettingScreen() {
-            // insert code here
-        }
-        public static void initiateBlackjack() {
-            // insert code here
-        }
-        public static void playerPhase() {
-            // insert code here
-        }
-        public static void cpuPhase() {
-            // insert code here
-        }
-        public static void resultsPhase() {
-            // insert code here
-        }
     }
+
+    /**
+     * Initialize the betting screen
+     */
+    public static void executeBettingScreen() {
+        // insert code here
+    }
+
+    /**
+     * have dealer give two cards to himself and you
+     */
+    public static void initiateBlackjack() {
+        Deck deck = new Deck();
+//        shuffleDeck(deck);
+        /** Give CPU card **/
+        /** Give Player card **/
+        /** Give CPU another card **/
+        /** Give Player card **/
+        // insert code here
+    }
+
+    /**
+     * User decides what to do with their cards
+     */
+    public static void playerPhase() {
+        // insert code here
+    }
+
+    /**
+     * CPU decides what to do with their hand/hands
+     */
+    public static void cpuPhase() {
+        // insert code here
+    }
+
+    /**
+     * Either give a winning screen or losing screen
+     */
+    public static void resultsPhase() {
+        // insert code here
+    }
+}
 
 
 
